fbb195bd23552d9d89c12cc3fcbce76091964b21
Krish-Panchani <krishpanchani1346@gmail.com>
2024-10-06 10:42:01 +0530

    Refactor Tailwind CSS configuration to remove unused font family extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'Nasa': ['Nasa'], // Adjust to your font name
-      },
-    },
+    extend: {},
     keyframes: {
       'gradient-animate': {
           '0%, 100%': {
